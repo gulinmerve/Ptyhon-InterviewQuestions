abc023e3fe79ac25220ce049bbb39997269a740f
Merve <gulineyel@gmail.com>
2020-10-17 13:08:18 +0300

    second answer

--- 17.Google.py
+++ 17.Google.py
@@ -1,5 +1,7 @@
 from random import choice
 def r_int(n, l):
     return choice([i for i in range(n) if i not in l])
-
-    
+#
+def rand_int(lst, n):
+    a=int(np.random.choice(range(0,n), 1))
+    return a if a not in lst else rand_int(lst, n)
