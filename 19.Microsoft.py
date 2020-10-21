e721f814e875b7e898c6a45470ee7473cf206df4
Merve <gulineyel@gmail.com>
2020-10-21 10:31:01 +0300

    solution 2 added

--- 19.Microsoft.py
+++ 19.Microsoft.py
@@ -1,27 +1,12 @@
-# Çözüm-1. sort ile listeyi sıralamaya izin varsa işler kolaylaşıyor.
-def longest(nums):
-    nums.sort()
-    d = dict()
-    for i in nums:
-        d[i] = i
-        if i-1 in d.keys():
-            d[i] = d[i-1]           
-    return (lambda y: y[0]-y[1]+1)(max(d.items(), key = lambda x: x[0]-x[1]))
+# Çözüm 1
+data = ["a", "b", "c", "d"]
+res=[""]
+for i in data:
+    res=[j+k for j in res for k in data if k not in j]
 
-
-# çözüm-2 listeyi sıralamadan, listenin mevcut hali ile çözüm
-def longest2(nums):
-    mx = 0
-    adj = dict()
-    for i in nums:
-        if i in adj:
-            continue
-        lst = [i]*3
-        if i - 1 in adj:
-            lst[0] = adj[i - 1][0]
-        if i + 1 in adj:
-            lst[2] = adj[i + 1][1]
-        for j in lst:
-            adj[j] = lst[0], lst[2]
-        mx = max(lst[2] - lst[0] + 1, mx)
-    return mx
+#Çözüm 2
+def perm(lst):    
+    r = [[]]
+    for _ in lst:
+        r = [i + [j] for i in r for j in lst if j not in i]
+    return r
