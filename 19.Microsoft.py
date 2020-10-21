f8fe4a76b4511a105c3dd2525a7f4e7a2912283f
Merve <gulineyel@gmail.com>
2020-10-21 10:27:44 +0300

    second solution

--- 19.Microsoft.py
+++ 19.Microsoft.py
@@ -7,3 +7,21 @@ def longest(nums):
         if i-1 in d.keys():
             d[i] = d[i-1]           
     return (lambda y: y[0]-y[1]+1)(max(d.items(), key = lambda x: x[0]-x[1]))
+
+
+# çözüm-2 listeyi sıralamadan, listenin mevcut hali ile çözüm
+def longest2(nums):
+    mx = 0
+    adj = dict()
+    for i in nums:
+        if i in adj:
+            continue
+        lst = [i]*3
+        if i - 1 in adj:
+            lst[0] = adj[i - 1][0]
+        if i + 1 in adj:
+            lst[2] = adj[i + 1][1]
+        for j in lst:
+            adj[j] = lst[0], lst[2]
+        mx = max(lst[2] - lst[0] + 1, mx)
+    return mx
