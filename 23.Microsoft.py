5232609605e7ce1170cbb0f34675135891f6ad47
Merve <gulineyel@gmail.com>
2020-10-23 13:33:18 +0300

    second solution

--- 23.Microsoft.py
+++ 23.Microsoft.py
@@ -8,3 +8,12 @@ for i in items:
         print((lst[half - 1] + lst[half]) / 2.0)
     else:
         print(lst[half]) 
+
+#
+def median(lst):
+    lst.sort()
+    mid = len(lst) // 2
+    return lst[mid] if len(lst) % 2 != 0 else (lst[mid] + lst[mid-1]) / 2 
+lst = [2, 1, 5, 7, 2, 0, 5]
+for i in range(1,len(lst)):
+    print(median(lst[:i]))
