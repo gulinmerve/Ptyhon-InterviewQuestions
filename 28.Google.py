ca4c80a5aee92a402f54ef76e66fc0a699721088
Merve <gulineyel@gmail.com>
2020-10-29 07:46:51 +0300

    solution-2

--- 28.Google.py
+++ 28.Google.py
@@ -1,3 +1,13 @@
 #Çözüm-1
 def non_duplicated_int(lst):
     return [i for i in lst if lst.count(i) == 1][0]
+
+#Çözüm-2
+def non_duplicated_int1(lst):
+    d = {}
+    for i in lst:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    return list(filter(lambda x: (d[x] == 1), d))[0]
