a558613080298089a7b7ea714806bb8a50dfb665
Merve <gulineyel@gmail.com>
2020-10-31 17:41:13 +0300

    sigma

---
+++ 32.sigma.py
@@ -0,0 +1,4 @@
+# rand5() fonksiyonu hazır kabul ediyoruz. Sonunun çözümünü fiziki olarak da görmek için aşağıdaki rand5() fonksiyonunu kullanabilirsiniz.
+from random import randint
+def rand5():
+    return randint(1,5)
