4f402abdc627011a336113d7d1cf7a9fff5e29a2
Merve <gulineyel@gmail.com>
2020-11-03 07:32:24 +0300

    sol2

--- 33.facebook.py
+++ 33.facebook.py
@@ -6,3 +6,15 @@ for i in range(len(lst)):
 		if lst[y]-lst[i] > maxdif and y > i:
 			maxdif = lst[y]-lst[i]
 print(maxdif)
+
+#Solution2
+lst = [1, 6, 19, 59, 30, 60]
+def buy_and_sell(lst):
+    max_profit = 0
+    min_price = lst[0]
+    for price in lst:
+        min_price = min(min_price, price)
+        compare_profit = price - min_price
+        max_profit = max(max_profit, compare_profit)
+    return max_profit
+print(buy_and_sell(lst))
