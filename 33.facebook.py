fdc827f4a165ab287ca2e9618e4b1bcdc231b4d6
Merve <gulineyel@gmail.com>
2020-11-03 07:33:10 +0300

    solution3

--- 33.facebook.py
+++ 33.facebook.py
@@ -18,3 +18,14 @@ def buy_and_sell(lst):
         max_profit = max(max_profit, compare_profit)
     return max_profit
 print(buy_and_sell(lst))
+
+#Solution3
+lst = [10, 20, 23, 22, 17, 30]
+def buy_and_sell(lst):
+    max_profit = 0
+    for i in range(len(lst)-1):
+        for j in range(i+1, len(lst)):
+            if lst[j] - lst[i] > max_profit:
+                max_profit = lst[j] - lst[i]
+    return max_profit
+print(buy_and_sell(lst))
