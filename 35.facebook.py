3a625fd90fa1442d03b2a5c4228956df0b6fbdad
Merve <gulineyel@gmail.com>
2020-11-05 21:21:27 +0300

    py

---
+++ 35.facebook.py
@@ -0,0 +1,11 @@
+def shuffle(x,k):
+    	deck = [i for i in range(1,k+1)]
+	for i in range(x):
+		x= randint(1,k)
+		while True:
+			y = randint(1,k)
+			if y != x:
+				break
+		deck[x-2], deck[y-2] = deck[y-2], deck[x-2]
+	return deck
+print(shuffle(500,52))
