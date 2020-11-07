9df9f3a652ee767e43c8dab80658506fa85bb7ed
Merve <gulineyel@gmail.com>
2020-11-07 19:33:30 +0300

    sol1

--- 37.target.py
+++ 37.target.py
@@ -4,3 +4,9 @@ def summ(nums, target):
         for j in [n for n in arr if n != i]:
             if i+j==target:
                 return sorted([nums.index(i), nums.index(j)])
+
+# Çözüm-1
+def twoSum1(nums, target):
+    for i in range(len(nums)):
+        if (target-nums[i] in nums) and nums.index(target-nums[i]) != i:
+            return [i, nums.index(target-nums[i])]
