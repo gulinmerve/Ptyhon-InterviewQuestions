d582636d123d99d5fe6b0093fa33645f23eb6e02
Merve <gulineyel@gmail.com>
2020-11-09 14:12:05 +0300

    second solution

--- 38.Easy.py
+++ 38.Easy.py
@@ -12,3 +12,21 @@ class Solution:
                     temp = j[i]
             res += temp
         return res
+
+# çözüm-1
+def longestCommonPrefix1(strs):
+    if len(strs) == 0:
+        return ""
+    elif len(strs) == 1:
+        return strs[0]
+    else:
+        short = min(strs,key = len)
+        result = ""
+        index = 0
+        while index < len(short):
+            for i in range(len(strs)):
+                if strs[i][index] != short[index]:
+                    return result
+            result += short[index]
+            index += 1
+        return result
