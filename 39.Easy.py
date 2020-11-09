4180f2acf652f45a5051638b057c5bf1c772a6aa
Merve <gulineyel@gmail.com>
2020-11-09 14:16:59 +0300

    solution added

---
+++ 39.Easy.py
@@ -0,0 +1,11 @@
+# Clarification:
+# Confused why the returned value is an integer but your answer is an array?
+# Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+# Internally you can think of this:
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
