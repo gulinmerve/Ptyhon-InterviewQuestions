7ece7d725922c8da9b6d294375f13f02991d3de3
Merve <gulineyel@gmail.com>
2020-11-12 21:39:13 +0300

    medşum2

---
+++ Medium.py
@@ -0,0 +1,11 @@
+def convert3(s, numRows):
+    if len(s) <= numRows or numRows == 1:
+        return s
+    table = [""]*numRows
+    r,dir = 0,-1
+    for i in s:
+        if (r == numRows-1) or (r == 0):
+            dir *= -1
+        table[r] += i
+        r += dir
+    return "".join(table)
