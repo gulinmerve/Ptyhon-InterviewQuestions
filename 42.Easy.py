d4ddd30276511ee08d6ae0bc2c0366ecbce49f4a
Merve <gulineyel@gmail.com>
2020-11-13 23:34:55 +0300

    length

---
+++ 42.Easy.py
@@ -0,0 +1,7 @@
+def last_word(words:str) -> int:
+    words_list = words.split()
+    if len(words_list) < 2:
+        return 0
+    else:
+        return len(words_list[-1])
+print(last_word("World"))
