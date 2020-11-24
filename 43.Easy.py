212517a4ee1efc923743bcb4a027729d93e6ba75
Merve <gulineyel@gmail.com>
2020-11-24 11:21:20 +0300

    easy-sol4

--- 43.Easy.py
+++ 43.Easy.py
@@ -23,3 +23,10 @@ def addone(arr):
     numback = num + 1 
     digits = [int(i) for i in str(numback)]  # convert to array
     return digits
+
+def inc(digits):
+    number=0
+    for i in range (len(digits)):
+        number+=digits[i]*(10**(len(digits)-1-i))
+    a=[int(i) for i in (list(str(number+1)))]
+    print (a)
