dcf6b690e5486351a9b89b0c69edcc6242cf20dd
Merve <gulineyel@gmail.com>
2020-11-26 21:39:56 +0300

    solution

--- 46.Easy.py
+++ 46.Easy.py
@@ -8,3 +8,10 @@ def reply(input):
         first = data[input // len(letters)]
         second = data[input % len(letters)]
         return first+second
+
+def convertToTitle(n):
+    result = ""
+    while n > 26:
+        result = chr(65+(n-1)%26)  + result  
+        n = (n-1) // 26
+    return chr(64+n) + result  
