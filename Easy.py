ddb6b5c683ef41d6b3492144217f855c77d0873e
Merve <gulineyel@gmail.com>
2020-11-28 22:10:03 +0300

    sloutiın

--- Easy.py
+++ Easy.py
@@ -2,3 +2,11 @@ for i in Input:
     if Input.count(i) > len(Input) / 2:
         print(i)
         break
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        x = set(nums)
+        for i in x:
+            if nums.count(i) > len(nums)/2:
+                return i
