812d66ae703ee0dd298cd26e75cdc670adeb3d7c
Merve <gulineyel@gmail.com>
2020-12-12 21:03:35 +0300

    edabit-

--- 51.EdabitChallenge2.py
+++ 51.EdabitChallenge2.py
@@ -7,3 +7,7 @@ def convert_to_decimal(perc):
         new_list.append(float(perc)*0.01)
     return new_list
 print(convert_to_decimal(["1%", "2%", "3%"]))
+
+def convert_to_decimal(perc):
+    	return [float(item[:-1])/100 for item in perc]
+convert_to_decimal(["1%", "2%", "3%"])
