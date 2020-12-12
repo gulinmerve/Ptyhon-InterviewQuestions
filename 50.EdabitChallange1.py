91ce6940f75fa19e32aa30386f111f36dc63d0c2
Merve <gulineyel@gmail.com>
2020-12-12 20:46:03 +0300

    edabit

--- 50.EdabitChallange1.py
+++ 50.EdabitChallange1.py
@@ -1,1 +1,10 @@
 #https://edabit.com/challenge/b8wRDMWgMZTN2nmfx
+
+def equal(a, b, c):
+    listed = [a, b, c]
+    repeat_list = [listed.count(i) for i in listed]
+    if max(repeat_list) == 1:
+        return 0
+    else:
+        return max(repeat_list)
+print(equal(2, 4, 5))
