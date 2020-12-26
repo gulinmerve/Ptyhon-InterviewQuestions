cc1188c567ae1c4bb4e6af5fb3939851284a9f23
Merve <gulineyel@gmail.com>
2020-12-26 18:08:06 +0300

    int-added

--- 52.int.py
+++ 52.int.py
@@ -13,3 +13,13 @@ ang = []
 for i in dict.values():
     ang.append(i)
 print(sorted(ang,key = len,reverse =True)) 
+
+a = {}
+for i in Input:
+    x = "".join(sorted(i))
+    if x in a:
+        a[x].append(i)
+    else:
+        a[x] = [i]
+b=list(a.values())
+print(b)
