7bb2ad9a345a8fb4ebe8a84d05eef3e09ca9b3af
Merve <gulineyel@gmail.com>
2020-12-26 18:07:16 +0300

    int

--- 52.int.py
+++ 52.int.py
@@ -1,16 +1,15 @@
-Facebook, Adobe ve Cisco gibi firmaların mülakatında çıkmış orta seviye bir Python sorusu :slightly_smiling_face:
-=============================
-Group Anagrams
-Given a list of strings, group anagrams together.
-Example:
-Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
-
-[
-  ["ate","eat","tea"],
-  ["nat","tan"],
-  ["bat"]
-]
-
-Note:
-All inputs will be in lowercase.
-The order of your output does not matter.
+x = []
+y = []
+other = []
+for i in input:
+     if 'b' in list(i):
+        x.append(i)
+    elif 'n' in list(i):
+         y.append(i)
+    else:
+        other.append(i)
+dict = {'x':x , 'y':y , 'other': other}
+ang = []
+for i in dict.values():
+    ang.append(i)
+print(sorted(ang,key = len,reverse =True)) 
