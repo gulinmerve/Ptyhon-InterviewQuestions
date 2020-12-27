faa2e212dbee6e1ea5370202d5e9afc86481ed38
Merve <gulineyel@gmail.com>
2020-12-27 10:47:19 +0300

    answer

--- 52.int.py
+++ 52.int.py
@@ -85,3 +85,33 @@ for i in mylist:
     if sorted(k) not in c:
         c.append(sorted(k))
 print(c)
+
+
+
+input = ['eat','tea','tan','ate','nat','bat']
+b = []
+for i in input:
+    a = []
+    for j in range(len(input)):
+        if set(i) == set(input[j]):
+            if input[j] not in a :
+                a.append(input[j])
+                a.sort()
+        else: continue   
+    if a in b: pass
+    else : b.append(a)
+print(b)
+
+
+def uniq(input):
+  items = []
+  for i in input:
+        if i not in items: items.append(i)
+  return items
+def anagram(input):
+    a,b=dict(),list()
+    for i in input: a[i]=''.join(sorted(i))
+    for j in uniq(a.values()): b.append(sorted([k for k,v in a.items() if v==j]))
+    return sorted(b,key=len,reverse=True)
+input=["eat", "tea", "tan", "ate", "nat", "bat"]
+anagram(input)
